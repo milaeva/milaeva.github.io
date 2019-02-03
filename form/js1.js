87f95b5ffd3a54e95f7bd187d35c407b59942f91
Vita Mileva <milayevavita27@gmail.com>
2019-02-03 16:20:01 +0200

    form

---
+++ form/js1.js
@@ -0,0 +1,58 @@
+var form = document.querySelector('.formWithValidation')
+var validateBtn = form.querySelector('.validateBtn')
+var firstName = form.querySelector('.firstName')
+var lastName = form.querySelector('.lastName')
+var birthday = form.querySelector('.birthday')
+var gender = form.querySelector('.gender')
+var country = form.querySelector('.country')
+var email = form.querySelector('.email')
+var password = form.querySelector('.password')
+var address = form.querySelector('.address')
+var notes = form.querySelector('.notes')
+
+
+var fields = form.querySelectorAll('.field')
+
+var generateError = function (text){
+	var error = document.createElement('div')
+      error.className = 'error'
+      error.style.color = 'red'
+      error.innerHTML = text
+      return error
+}
+
+var removeValidation = function (){
+	var errors = form.querySelectorAll('.error')
+
+  for (var i = 0; i < errors.length; i++) {
+    errors[i].remove()
+  }
+}
+
+var checkFieldsPresence = function (){
+	 for (var i = 0; i < fields.length; i++) {
+    if (!fields[i].value) {
+      console.log('This field is required', fields[i])
+      var error = generateError ('Cannot be blank')
+      form[i].parentElement.insertBefore(error, fields[i])
+    }
+  }
+}
+
+var checkPasswordMatch = function() {
+	if (password.value !== password.value) {
+    console.log('This field is not valid')
+   var error = generateError ('This field is not valid')
+    password.parentElement.insertBefore(error, password)
+  }
+}
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault()
+
+  removeValidation()
+	
+	checkFieldsPresence()
+
+  checkPasswordMatch()
+})
